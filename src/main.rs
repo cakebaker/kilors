8c6c198a696cf31045489d734d178beec24705c6
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2020-03-20 14:49:53 +0100

    Read and display a file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![warn(clippy::all, clippy::pedantic)]
-
+mod document;
 mod editor;
+mod row;
 mod terminal;
 
+use document::Document;
 use editor::Editor;
+use row::Row;
 use terminal::Terminal;
 use editor::Position;
 
